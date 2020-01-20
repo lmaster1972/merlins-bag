717b35aaab5f9084f28c602622382f44918bfafc
Luca Mastrobuono <51300165+lmaster1972@users.noreply.github.com>
2020-01-20 13:39:36 +0100

    Add files via upload

--- merlins-bag.sh
+++ merlins-bag.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
+# first compress regular files
+find ./ -type f -execdir tar -zrvf root.tar.gz {} \;
+
+# then compress the subfolders
 for dir in *; do tar czvf "$dir".tar.gz "$dir";done
